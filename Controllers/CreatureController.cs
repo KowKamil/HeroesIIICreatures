79fb1904f4544171bcdbfe9caac075c7620a1ab4
Kamil K <gkowalski.kamil@gmail.com>
2020-10-12 12:40:52 +0200

    Removed stock Angular from the project

--- Controllers/CreatureController.cs
+++ Controllers/CreatureController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-using Heroes_3_Creatures.Data;
 using Heroes_3_Creatures.Dtos;
 using Heroes_3_Creatures.Models;
 using Microsoft.AspNetCore.Mvc;
--- Startup.cs
+++ Startup.cs
@@ -31,11 +31,6 @@ namespace Heroes_3_Creatures
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,21 +60,9 @@ namespace Heroes_3_Creatures
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
+                    pattern: "{controller=Creature}/{action=GetAll}/{id?}");
             });
 
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
-            });
         }
     }
 }
